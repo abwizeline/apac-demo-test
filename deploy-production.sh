cfba6472e93212eca4fa356aa95b6ef8ae55f941
Artem Bogomaz <artembogomaz@gmail.com>
2019-12-02 11:25:19 +0700

    travis 11

--- deploy-production.sh
+++ deploy-production.sh
@@ -21,6 +21,7 @@ gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 echo "1"
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 echo "2"
+echo gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:latest
