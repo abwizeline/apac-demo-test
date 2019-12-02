eecf9787d6e7e3cf45f468fa2b032699045fba5a
Artem Bogomaz <artembogomaz@gmail.com>
2019-12-02 11:07:13 +0700

    travis fix 8

--- deploy-production.sh
+++ deploy-production.sh
@@ -12,6 +12,12 @@ gcloud --quiet config set project $PROJECT_NAME_PRD
 gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 
+gcloud container clusters create ${CLUSTER_NAME_PRD} \
+  --num-nodes ${NODES_NUM} \
+  --machine-type ${GCLOUD_MACHINE_TYPE} \
+  --zone ${CLOUDSDK_COMPUTE_ZONE}
+
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:latest
@@ -19,12 +25,6 @@ yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_I
 kubectl config view
 kubectl config current-context
 
-gcloud container clusters create ${CLUSTER_NAME_PRD} \
-  --num-nodes ${NODES_NUM} \
-  --machine-type ${GCLOUD_MACHINE_TYPE} \
-  --zone ${CLOUDSDK_COMPUTE_ZONE}
-
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
 ## kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 echo "start deployment.yaml"
