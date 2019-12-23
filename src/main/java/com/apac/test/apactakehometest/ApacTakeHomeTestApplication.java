f8336144e9b6661301d480164a0102ca87408d68
Artem Olegovich <artembogomaz@gmail.com>
2019-12-23 13:26:58 +0700

    dates fix

--- src/main/java/com/apac/test/apactakehometest/ApacTakeHomeTestApplication.java
+++ src/main/java/com/apac/test/apactakehometest/ApacTakeHomeTestApplication.java
@@ -19,7 +19,7 @@ public class ApacTakeHomeTestApplication {
 
     @PostConstruct
     public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone("EDT"));
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
     }
 
 }
